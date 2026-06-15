b3b7fabf8c508c17009ec50b3f10ba81dd02d6e7
tgdmaah1 <ahmedsami.maklad@swisscom.com>
2026-06-15 19:16:34 +0200

    fix CORS issue

--- node_helper.js
+++ node_helper.js
@@ -2,6 +2,8 @@
  * Node Helper: MMM-PrayerTime
  *
  * By Slamet PS/slametps@gmail.com
+ * This is an optional helper that will be loaded by the node script. The node helper and module script can communicate with each other using an integrated socket system.
+ * The node helper (node_helper.js) is a Node.js script that is able to do some backend task to support your module.
  * MIT Licensed.
  */
 
@@ -14,6 +16,46 @@ module.exports = NodeHelper.create({
 	start: function() {
 		console.log("Starting node_helper.js for MMM-PrayerTime.");
 	},
+
+  getPTonline(url){
+    // new Date(Date.now())
+    Log.info(this.name + ": Fetching prayer times from " + url);
+    todayRequest=new XMLHttpRequest();
+		todayRequest.open("GET", url, true);
+		todayRequest.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+          resultToday = JSON.parse(this.responseText);
+          Log.info(self.name + ": Prayer times received: " + JSON.stringify(resultToday.data.timings));
+          self.todaySchedule = resultToday.data.timings;
+          // debug/testing only
+          //self.todaySchedule = {"Fajr":"04:30", "Dhuhr":"12:00", "Asr":"16:14", "Maghrib":"18:00", "Isha":"20:50", "Imsak":"04:20"};
+          nbRes++;
+          if (nbRes == nbReq)
+            self.processSchedule();
+				} else {
+					Log.error(self.name + ": got HTTP status-" + this.status);
+          retry = true;
+				}
+			}
+		};
+		todayRequest.send();
+    return(todayRequest);
+
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.sendSocketNotification("PT_RESULT", data);
+    //   })
+    //   .catch(error => {
+    //     console.error("Error fetching prayer times:", error);
+    //   });
+  },
+
+  getPTOffline(){
+    var ptData = require('./prayer-time.json');
+    this.sendSocketNotification("PT_RESULT", ptData);
+  },
 
 	socketNotificationReceived: function(notification, payload) {
     console.log(this.name + " node helper received a socket notification: " + notification + " - Payload: " + payload);
